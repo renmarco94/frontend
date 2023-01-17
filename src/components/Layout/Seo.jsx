f93a47279e03f53a696b6253ec4f6579dd6a479e
renmarco94 <remarco94@proton.me>
2023-01-17 11:59:29 +0100

    fused tag

--- src/components/Layout/Seo.jsx
+++ src/components/Layout/Seo.jsx
@@ -24,10 +24,10 @@ export default function Seo({ title, description, pathname, children }) {
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={seo.title} />
+      <meta name="og:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
-      <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.image} />
+      <meta name="og:description" content={seo.description} />
+      <meta name="og:image" content={seo.image} />
       <meta name="twitter:creator" content={seo.twitterUsername} />
       <link rel="icon" href={seo.image} />
       {children}
