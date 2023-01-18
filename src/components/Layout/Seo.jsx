c384061113198118755976524def5b13ab2ccb97
renmarco94 <remarco94@proton.me>
2023-01-18 13:13:49 +0100

    face tag mod

--- src/components/Layout/Seo.jsx
+++ src/components/Layout/Seo.jsx
@@ -36,11 +36,10 @@ export default function Seo({ title, description, pathname, children }) {
         <meta property="og:url" content={seo.url} />
         {/* <meta name="twitter:image" content={seo.image} /> */}
         <meta property="og:image" content={seo.image} />
+        <meta property="og:type" content="website" />
         <link rel="icon" href={seo.icon} />
         {children}
       </>
     </>
   )
 }
-
-// < metaproperty="og: url"content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"/>< metaproperty="og: type"content="article"/>< metaproperty="og: title"content="When Great Minds Don’t Think Alike"/>< metaproperty="og: description"content="How much does culture influence creative thinking?"/><goalproperty="og: image"content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"/>
